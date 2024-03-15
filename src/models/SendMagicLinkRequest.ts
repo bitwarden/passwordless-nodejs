661a989e12182ba1763303fae4a20543dc807ab1
Jared McCannon <jared.r.mccannon@gmail.com>
2024-03-15 05:47:23 -0500

    Adding ttl to the magic links and generate auth token request objects. (#34)

--- src/models/SendMagicLinkRequest.ts
+++ src/models/SendMagicLinkRequest.ts
@@ -1,8 +1,14 @@
 export class SendMagicLinkRequest {
-  constructor(emailAddress: string, urlTemplate: string, userId: string) {
+  constructor(
+    emailAddress: string,
+    urlTemplate: string,
+    userId: string,
+    timeToLive?: number,
+  ) {
     this.emailAddress = emailAddress;
     this.urlTemplate = urlTemplate;
     this.userId = userId;
+    this.timeToLive = timeToLive;
   }
 
   emailAddress: string;
